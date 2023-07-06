0ccd9c62232ce2f64aaa8ee808fd2df0a8cdd7ed
Edmond Setati <NovemberXXI.Business@gmail.com>
2023-07-06 17:20:11 +0200

    working on nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import EMSLogo from "../assets/EMSLogo.png";
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className="fixed flex w-full h-[80px] justify-between items-center px-4 text-gray-300">
+      <div>
+        <img src={EMSLogo} alt="my logo" style={{ width: "50px" }} />
+      </div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Skills</li>
+          <li>Contact</li>
+        </ul>
+      </div>
 
+      {/* h menu */}
+      <div>
+        <FaBars />
+      </div>
+      {/* mobile menu */}
+      <ul className="hidden">
+        <li>Home</li>
+        <li>About</li>
+        <li>My Work</li>
+        <li>Skills</li>
+        <li>Contact</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
