af12ceaecc83e8c61102e2ad8e39349a39db5f3c
Edmond Setati <NovemberXXI.Business@gmail.com>
2023-07-07 20:09:58 +0200

    hero section finished

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-gradient-to-b from-sky-900 via-sky-900 to-orange-800'>
+        {/* conta */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-black text-xl '>Hello, my name is</p>
+            <h1 className='text-5xl sm:text-7xl py-2 font-bold text-[#ccd6f6]'>Edmond Setati</h1>
+            <h2 className='text-4xl sm:text-7xl text-[#8892b0]'>I'm a Front-end web Developer.</h2>
+            <p className='text-black text-xl py-4 max-w-[600px] ml-10'>I am driven by curiosity and armed with a solid foundation in HTML, CSS, and JavaScript, I am also eager to apply my knowledge and creativity to build impactful web experiences. While still in the early stages of my web development career, I am enthusiasm and dedication shine through in every project.</p>
+
+            <div>
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center rounded-md hover:bg-gradient-to-r from-sky-500 to-orange-700'>My Work 
+                <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-3'/></span>
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,72 @@
-import React from "react";
-import { FaBars } from "react-icons/fa";
+import React, {useState} from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 import EMSLogo from "../assets/EMSLogo.png";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+
+  const handleClick = () => setNav(!nav)
+
   return (
-    <div className="fixed flex w-full h-[80px] justify-between items-center px-4 bg-gradient-to-b from-orange-600 to-sky-900 text-gray-300">
+    <div className="fixed flex w-full h-[80px] justify-between items-center px-4 bg-gradient-to-b from-orange-900 to-sky-900 text-gray-100">
       <div>
         <img src={EMSLogo} alt="my logo" style={{ width: "50px" }} />
       </div>
-      <div>
-        <ul>
+
+      {/* menu */}
+      
+        <ul className="hidden md:flex">
           <li>Home</li>
           <li>About</li>
           <li>Projects</li>
           <li>Skills</li>
           <li>Contact</li>
         </ul>
-      </div>
+
 
       {/* h menu */}
-      <div>
-        <FaBars />
+      <div onClick={handleClick} className="md:hidden z-10" >
+        {!nav ? <FaBars /> : <FaTimes/> }
       </div>
+
+
       {/* mobile menu */}
-      <ul className="hidden">
-        <li>Home</li>
-        <li>About</li>
-        <li>My Work</li>
-        <li>Skills</li>
-        <li>Contact</li>
+      <ul className={!nav ? 'hidden' : "absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-orange-900 to-sky-900 flex flex-col justify-center items-center"}>
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">My Work</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Contact</li>
       </ul>
+
+      {/* social icons */}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 rounded-t-lg">
+            <a href="https://www.linkedin.com/in/edmond-marumo-setati-451642192/" className="flex justify-between items-center w-full text-gray-200">
+              LinkedIn <FaLinkedin size={30}/>
+            </a>
+          </li>
+
+          <li className="w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-800">
+            <a href="https://GitHub.com/NovemberXXI" className="flex justify-between items-center w-full text-gray-200">
+              Github <FaGithub size={30}/>
+            </a>
+          </li>
+          <li className="w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-red-700">
+            <a href="mailto:SirEMSetati@gmail.com" className="flex justify-between items-center w-full text-gray-200">
+              Email <HiOutlineMail size={30}/>
+            </a>
+          </li>
+          <li className="w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-500 rounded-b-lg">
+            <a href="/public/EdmondMarumoSetati.pdf" className="flex justify-between items-center w-full text-gray-200">
+              Resume <BsFillPersonLinesFill size={30}/>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
